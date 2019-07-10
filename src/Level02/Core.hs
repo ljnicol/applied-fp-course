7c61aa5c926695de7e238c4a9f90af341d246402
ljnicol <l.nicol.9@gmail.com>
2019-07-10 14:14:40 +1000

    Finished level 2

--- src/Level02/Core.hs
+++ src/Level02/Core.hs
@@ -1,58 +1,53 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Level02.Core (runApp, app) where
 
-import           Network.Wai              (Application, Request, Response,
-                                           pathInfo, requestMethod, responseLBS,
-                                           strictRequestBody)
-import           Network.Wai.Handler.Warp (run)
+module Level02.Core
+  ( runApp
+  , app
+  ) where
 
-import           Network.HTTP.Types       (Status, hContentType, status200,
-                                           status400, status404)
+import           Network.Wai                (Application, Request, Response,
+                                             lazyRequestBody, pathInfo,
+                                             requestMethod, responseLBS,
+                                             strictRequestBody)
+import           Network.Wai.Handler.Warp   (run)
 
-import qualified Data.ByteString.Lazy     as LBS
+import           Network.HTTP.Types         (Status, hContentType, status200,
+                                             status400, status404)
 
-import           Data.Either              (either)
+import           Network.HTTP.Types.Method  (StdMethod (..), parseMethod)
 
-import           Data.Text                (Text)
-import           Data.Text.Encoding       (decodeUtf8)
+import qualified Data.ByteString.Lazy       as LBS
+import qualified Data.ByteString.Lazy.Char8 as LBSchar
 
-import           Level02.Types            (ContentType, Error, RqType,
-                                           mkCommentText, mkTopic,
-                                           renderContentType)
+import           Data.Either                (either)
+
+import           Data.Text                  (Text)
+import           Data.Text.Encoding         (decodeUtf8)
+
+import           Level02.Types              (ContentType (..), Error (..),
+                                             RqType (..), errorToMessage,
+                                             mkCommentText, mkTopic,
+                                             renderContentType)
 
 -- |-------------------------------------------|
 -- |- Don't start here, go to Level02.Types!  -|
 -- |-------------------------------------------|
-
 -- | Some helper functions to make our lives a little more DRY.
-mkResponse
-  :: Status
-  -> ContentType
-  -> LBS.ByteString
-  -> Response
-mkResponse =
-  error "mkResponse not implemented"
+mkResponse :: Status -> ContentType -> LBS.ByteString -> Response
+mkResponse status contentType responseText =
+  responseLBS
+    status
+    [("Content-Type", renderContentType contentType)]
+    responseText
 
-resp200
-  :: ContentType
-  -> LBS.ByteString
-  -> Response
-resp200 =
-  error "resp200 not implemented"
+resp200 :: ContentType -> LBS.ByteString -> Response
+resp200 = mkResponse status200
 
-resp404
-  :: ContentType
-  -> LBS.ByteString
-  -> Response
-resp404 =
-  error "resp404 not implemented"
+resp404 :: ContentType -> LBS.ByteString -> Response
+resp404 = mkResponse status404
 
-resp400
-  :: ContentType
-  -> LBS.ByteString
-  -> Response
-resp400 =
-  error "resp400 not implemented"
+resp400 :: ContentType -> LBS.ByteString -> Response
+resp400 = mkResponse status400
 
 -- |----------------------------------------------------------------------------------
 -- These next few functions will take raw request information and construct         --
@@ -63,49 +58,55 @@ resp400 =
 -- and verify. It also allows for greater reuse and it also means that              --
 -- validation is not duplicated across the application, maybe incorrectly.          --
 --------------------------------------------------------------------------------------
-
-mkAddRequest
-  :: Text
-  -> LBS.ByteString
-  -> Either Error RqType
-mkAddRequest =
-  error "mkAddRequest not implemented"
-  where
+mkAddRequest :: Text -> LBS.ByteString -> Either Error RqType
+mkAddRequest inputText bsComment
+  -- (<*>)
+  --   (fmap (AddRq) (mkTopic inputText))
+  --   (mkCommentText $ lazyByteStringToStrictText bsComment)
+ =
+  AddRq <$> (mkTopic inputText) <*>
+  (mkCommentText $ lazyByteStringToStrictText bsComment)
+  -- case (mkTopic inputText, mkCommentText $ lazyByteStringToStrictText bsComment) of
+  --   (Right topic, Right comment) -> Right $ AddRq topic comment
+  --   (Left topicE, _)             -> Left topicE
+  --   (_, Left commentE)           -> Left commentE
+  -- using fmap!!!
+  -- fmap :: (a -> b) -> f a -> f b
+  -- (<*>) :: f (a -> b ) -> f a -> f b
+  --
+  -- ($) :: (a -> b) -> a -> b
+  -- (<$>) :: (a -> b ) -> f a -> f b
+  -- (<*>) :: f (a -> b) -> f a -> f b
     -- This is a helper function to assist us in going from a Lazy ByteString, to a Strict Text
-    lazyByteStringToStrictText =
-      decodeUtf8 . LBS.toStrict
+  where
+    lazyByteStringToStrictText = decodeUtf8 . LBS.toStrict
 
-mkViewRequest
-  :: Text
-  -> Either Error RqType
-mkViewRequest =
-  error "mkViewRequest not implemented"
+mkViewRequest :: Text -> Either Error RqType
+mkViewRequest inputText = fmap ViewRq (mkTopic inputText)
 
-mkListRequest
-  :: Either Error RqType
-mkListRequest =
-  error "mkListRequest not implemented"
+mkListRequest :: Either Error RqType
+mkListRequest = Right ListRq
 
 -- |----------------------------------
 -- end of RqType creation functions --
 --------------------------------------
-
-mkErrorResponse
-  :: Error
-  -> Response
-mkErrorResponse =
-  error "mkErrorResponse not implemented"
+mkErrorResponse :: Error -> Response
+mkErrorResponse e = resp400 PlainText (errorToMessage e)
 
 -- | Use our ``RqType`` helpers to write a function that will take the input
 -- ``Request`` from the Wai library and turn it into something our application
 -- cares about.
-mkRequest
-  :: Request
-  -> IO ( Either Error RqType )
-mkRequest =
+mkRequest :: Request -> IO (Either Error RqType)
+mkRequest request =
+  case (parseMethod . requestMethod $ request, pathInfo request) of
+    (Right POST, topic:"add":[]) -> do
+      comment <- lazyRequestBody request
+      pure $ mkAddRequest topic comment
+    (Right GET, topic:"view":[]) -> pure $ mkViewRequest topic
+    (Right GET, "list":[]) -> pure $ mkListRequest
+    _ -> pure $ Left InvalidRequest
   -- Remembering your pattern-matching skills will let you implement the entire
   -- specification in this function.
-  error "mkRequest not implemented"
 
 -- | If we find that we need more information to handle a request, or we have a
 -- new type of request that we'd like to handle then we update the ``RqType``
@@ -118,17 +119,31 @@ mkRequest =
 -- For now, return a made-up value for each of the responses as we don't have
 -- any persistent storage. Plain text responses that contain "X not implemented
 -- yet" should be sufficient.
-handleRequest
-  :: RqType
-  -> Either Error Response
-handleRequest =
-  error "handleRequest not implemented"
+handleRequest :: RqType -> Either Error Response
+handleRequest (AddRq topic comment) =
+  Right $
+  resp200
+    PlainText
+    (LBSchar.pack
+       ("Add request not implemented " ++ show topic ++ " " ++ show comment))
+handleRequest (ViewRq topic) =
+  Right $
+  resp200
+    PlainText
+    (LBSchar.pack $ ("View request not implemented" ++ show topic))
+handleRequest ListRq =
+  Right $ resp200 PlainText ("List request not implemented")
 
 -- | Reimplement this function using the new functions and ``RqType`` constructors as a guide.
-app
-  :: Application
-app =
-  error "app not reimplemented"
+app :: Application
+app inputRequest respond = do
+  request <- mkRequest inputRequest
+  -- use bind: (>>=) :: m a -> (a -> m b) -> m b
+  -- in this case (m = Either Error): Either Error RqType -> (RqType -> Either Error Response) -> Either Error Response
+  let a = request >>= handleRequest
+  -- either :: (a -> c) -> (b -> c) -> Either a b -> c
+  -- takes care of the case statement pattern match.
+  respond $ either mkErrorResponse id a
 
 runApp :: IO ()
 runApp = run 3000 app
--- src/Level02/Types.hs
+++ src/Level02/Types.hs
@@ -1,54 +1,53 @@
-{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -fno-warn-dodgy-exports #-}
+
 module Level02.Types
   ( Topic
   , CommentText
-  , ContentType (..)
-  , RqType (..)
-  , Error (..)
+  , ContentType(..)
+  , RqType(..)
+  , Error(..)
   , mkTopic
   , getTopic
   , mkCommentText
   , getCommentText
   , renderContentType
+  , errorToMessage
   ) where
 
-import           Data.ByteString (ByteString)
-import           Data.Text       (Text)
+import           Data.ByteString      (ByteString)
+import qualified Data.ByteString.Lazy as LBS
+import           Data.Text            as Text
 
 -- Working through the specification for our application, what are the
 -- types of requests we're going to handle?
-
 -- We have to be able to:
 -- - Comment on a given topic
 -- - View a topic and its comments
 -- - List the current topics
-
 -- The first thing we can do is provide some safety and structure around our
 -- ``Topic`` and ``CommentText`` types. Since we don't want to deal with bare
 -- ``Text`` values, we'll define some ``newtype`` wrappers for these types to
 -- make our code easier to work with.
-
 -- In Haskell the ``newtype`` is a wrapper of sorts that comes with zero runtime
 -- cost. It is purely used for type-checking as it constructs an entirely new
 -- type, not just a type alias. So when you have a bare primitive value, like an
 -- ``Int``, ``String``, or even ``[a]``, you can wrap it up in a ``newtype`` to
 -- give it a descriptive name to be more precise in your types.
-
 -- The type system will check it for you, and the compiler will eliminate the
 -- cost of the "wrapper". Also, having specialised constructor functions for the
 -- newtypes allows you to set extra restrictions, such as minimum values.
-
 -- The ``newtype`` definitions for ``Topic`` and ``CommentText`` are provided
 -- for you:
-
 -- Topic
-newtype Topic = Topic Text
-  deriving Show
+newtype Topic =
+  Topic Text
+  deriving (Show)
 
 -- CommentText
-newtype CommentText = CommentText Text
-  deriving Show
+newtype CommentText =
+  CommentText Text
+  deriving (Show)
 
 -- Using these convenient definitions, we can create the following constructors
 -- for our RqType:
@@ -57,14 +56,24 @@ newtype CommentText = CommentText Text
 -- ViewRq : Which needs the topic being requested.
 -- ListRq : Which doesn't need anything and lists all of the current topics.
 data RqType
+  = AddRq Topic
+          CommentText
+  | ViewRq Topic
+  | ListRq
 
 -- Not everything goes according to plan, but it's important that our types
 -- reflect when errors can be introduced into our program. Additionally it's
 -- useful to be able to be descriptive about what went wrong.
-
 -- Fill in the error constructors as you need them.
 data Error
+  = EmptyTopic
+  | EmptyComment
+  | InvalidRequest
 
+errorToMessage :: Error -> LBS.ByteString
+errorToMessage EmptyTopic     = "Missing topic"
+errorToMessage EmptyComment   = "Missing comment"
+errorToMessage InvalidRequest = "Invalid Request"
 
 -- Provide the constructors for a sum type to specify the `ContentType` Header,
 -- to be used when we build our Response type. Our application will be simple,
@@ -73,6 +82,8 @@ data Error
 -- - plain text
 -- - json
 data ContentType
+  = PlainText
+  | Json
 
 -- The ``ContentType`` constructors don't match what is required for the header
 -- information. Because ``wai`` uses a stringly type. So write a function that
@@ -85,11 +96,9 @@ data ContentType
 -- - plain text = "text/plain"
 -- - json       = "application/json"
 --
-renderContentType
-  :: ContentType
-  -> ByteString
-renderContentType =
-  error "renderContentType not implemented"
+renderContentType :: ContentType -> ByteString
+renderContentType PlainText = "text/plain"
+renderContentType Json      = "application/json"
 
 -- We can choose to *not* export the constructor for a data type and instead
 -- provide a function of our own. In our case, we're not interested in empty
@@ -98,29 +107,21 @@ renderContentType =
 --
 -- The export list at the top of this file demonstrates how to export a type,
 -- but not export the constructor.
+mkTopic :: Text -> Either Error Topic
+-- mkTopic "" = Left EmptyTopic
+-- mkTopic t = Right (Topic t)
+-- same thing with "guard syntax":
+mkTopic t
+  | Text.null t = Left EmptyTopic
+  | otherwise = Right (Topic t)
 
-mkTopic
-  :: Text
-  -> Either Error Topic
-mkTopic =
-  error "mkTopic not implemented"
+getTopic :: Topic -> Text
+getTopic (Topic a) = a
 
-getTopic
-  :: Topic
-  -> Text
-getTopic =
-  error "getTopic not implemented"
+mkCommentText :: Text -> Either Error CommentText
+mkCommentText "" = Left EmptyComment
+mkCommentText t  = Right (CommentText t)
 
-mkCommentText
-  :: Text
-  -> Either Error CommentText
-mkCommentText =
-  error "mkCommentText not implemented"
-
-getCommentText
-  :: CommentText
-  -> Text
-getCommentText =
-  error "getCommentText not implemented"
-
+getCommentText :: CommentText -> Text
+getCommentText (CommentText a) = a
 ---- Go to `src/Level02/Core.hs` next
